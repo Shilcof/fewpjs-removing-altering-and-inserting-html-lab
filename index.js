cc00faac7e7237ba496d2946d39e1e17e482e328
Shilcof <robert.shilcof@gmail.com>
2021-01-25 16:33:39 +0000

    commit tree 55a54973b4aa48827f39883ee3c062428ba43dc9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.className = 'victory';
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
+
+// 1) no longer has DOM node 'main#main'
+// 2) has a 'newHeader' variable that points to node 'h1#victory'
+// 3) has a 'newHeader' variable that points to node 'h1#victory'
+// 4) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
